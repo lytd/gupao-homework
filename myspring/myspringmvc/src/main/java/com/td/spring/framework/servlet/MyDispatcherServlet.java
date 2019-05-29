d7bd8df6a2696bbf640815dcc0b885b453596ac1
madi <madi901114@gmail.com>
2019-05-29 17:14:02 +0800

    myspring start

--- myspring/myspringmvc/src/main/java/com/td/spring/framework/servlet/MyDispatcherServlet.java
+++ myspring/myspringmvc/src/main/java/com/td/spring/framework/servlet/MyDispatcherServlet.java
@@ -1,11 +1,12 @@
 package com.td.spring.framework.servlet;
 
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+import java.util.Properties;
 
 public class MyDispatcherServlet extends HttpServlet {
 
@@ -13,20 +14,28 @@ public class MyDispatcherServlet extends HttpServlet {
         super();
     }
 
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
-    }
+    private final static String scanPackage = "scanPackage";
+    //全局配置
+    private final static Properties properties=new Properties();
+
+
+
+
+
+
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-       super.init(config);
 
-       String propertiesLocation = config.getInitParameter("contextConfigLocation");
+        String propertiesLocation = config.getInitParameter("contextConfigLocation");
 
         // 获取配置文件
 
+        getProperties(propertiesLocation);
+
         // 扫描类文件
+        doScan();
+
 
         // 添加到IOC容器
 
@@ -37,6 +46,48 @@ public class MyDispatcherServlet extends HttpServlet {
         //controller调用
 
         //返回结果
+
+    }
+
+    private void doScan() {
+        //获取基础包路径
+
+        String base = properties.getProperty(scanPackage);
+
+        String rootPath="/" + base.replaceAll("\\.", "/");
+        //获取根路径地址
+        URL url = this.getClass().getClassLoader().getResource(rootPath);
+
+
+        File classRootPath=new File(url.getFile());
+
+        if (classRootPath.isDirectory()){
+
+            for(File file:classRootPath.listFiles()){
+
+
+            }
+
+        }
+
+
+        //扫描
+
+
+    }
+
+    private void getProperties(String propertiesLocation) {
+
+        propertiesLocation = propertiesLocation.split(":")[1];
+
+        InputStream io = this.getClass().getClassLoader().getResourceAsStream(propertiesLocation);
+
+        try{
+            properties.load(io);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
